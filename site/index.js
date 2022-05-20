cc32f17d2718529d60cc0312d08f3e3232d038c1
Ethan Heavey <65369063+VeiledTee@users.noreply.github.com>
2022-05-19 21:25:53 -0600

    Update index.js

--- site/index.js
+++ site/index.js
@@ -1,4 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0.build/three.module.js'
+import * as THREE from 'three'
+
 
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
