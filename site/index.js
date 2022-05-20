14246ed7cb167fb9bbf4d3732d842b1a7fb421a3
Ethan Heavey <65369063+VeiledTee@users.noreply.github.com>
2022-05-19 21:22:16 -0600

    Update index.js

--- site/index.js
+++ site/index.js
@@ -7,7 +7,7 @@ const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00
 })
-const mesh = THREE.Mesh(geometry.material)
+const mesh = new THREE.Mesh(geometry.material)
 scene.add(mesh)
 
 // boilerplate
