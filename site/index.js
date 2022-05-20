2e66f839b1cfdc5ed42ea3f1145314fa81fe5a31
Ethan Heavey <65369063+VeiledTee@users.noreply.github.com>
2022-05-19 21:30:35 -0600

    Update index.js

--- site/index.js
+++ site/index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+import * as THREE from 'https://unpkg.com/three@0.130.0/build/three.module.js'
 
 
 const canvas = document.querySelector('.webgl')
